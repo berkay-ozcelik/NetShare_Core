cec7ee78da937687d582dc89db11fcde371327fb
Berkay Ozcelik <Berkay.Ozcelik@logo.com.tr>
2023-06-09 20:38:21 +0300

    Moving error fixed for CommandResult

--- Entity/CommandResult.cs
+++ Entity/CommandResult.cs
@@ -6,7 +6,44 @@ using System.Threading.Tasks;
 
 namespace NetShare_Core.Entity
 {
-    internal class CommandResult
+    public class CommandResult
     {
+
+        public enum ResultType
+        {
+            Success,
+            Error
+        }
+
+        public ResultType Type { get; }
+        public string Message { get; }
+
+        private CommandResult(ResultType type, string message)
+        {
+            Type = type;
+            Message = message;
+        }
+
+
+        public static CommandResult Success(string message)
+        {
+            return new CommandResult(ResultType.Success, message);
+        }
+
+        public static CommandResult Success()
+        {
+            return new CommandResult(ResultType.Success, string.Empty);
+        }
+
+        public static CommandResult Error(string message)
+        {
+            return new CommandResult(ResultType.Error, message);
+        }
+
+        public static CommandResult Error()
+        {
+            return new CommandResult(ResultType.Error, string.Empty);
+        }
+
     }
 }
